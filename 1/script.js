97553cb4bb81cf37982e68f894185d6933d21cb3
rostyslav_t <rostyslav.tovstenko@playtech.com>
2021-03-30 19:39:28 +0300

    fetch

--- 1/script.js
+++ 1/script.js
@@ -1,1 +1,13 @@
-alert("Hello")
+//alert("Hello")
+console.log("Hi.");
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(resp => resp.json())
+    .then(resp => {
+        console.log(resp)
+        for (let i in resp) {
+            const d = resp[i]
+            //console.log(d)
+            document.querySelector('#data_field').innerHTML += '<p>' + d.name + ' from ' + d.address.city + '</p>'
+        }
+
+    })
